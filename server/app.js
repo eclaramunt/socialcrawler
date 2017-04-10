e2eee2466bd509d520e84e1043f69afc8558f839
Ezequiel Claramunt <ezequiel.claramunt@gmail.com>
2017-04-10 20:45:53 -0300

    Archivos de configuracion por ambiente, mongoose como gesto de BD

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+var config = require('./config');
 
 // routes
 var router = require('./router')(app)
+
+// mongoose
+var mongoose = require('mongoose');
+var urlMongoose = config.Mongo.client + "://" + config.Mongo.host + ":" + config.Mongo.port + "/" + config.Mongo.dbName;
+
+mongoose.connect(urlMongoose, function (err) {
+    if (err) throw err;
+});
 
 // Error Handling
 app.use(function (err, req, res, next) {
