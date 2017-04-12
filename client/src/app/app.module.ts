e1b7b01885669bec71eddcdf8cab259bc2018154
Ezequiel Claramunt <ezequiel@sthventures.com>
2017-04-12 10:28:25 -0300

    Authentication de usuarios en Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,36 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// rutas
+import { routing } from './app.routing';
+
+// interceptores
+import { AuthGuard } from './_guards/auth.guards';
+
+// components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+// services
+import { AuthenticationService } from './_services/authentication.services';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AuthenticationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
