41e236451fc05c7aed68ccd3034c63fa303daa26
Ezequiel Claramunt <ezequiel@sthventures.com>
2017-08-15 14:43:26 -0300

    Alta de cuentas para Facebook

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -7,7 +7,7 @@ var logger = new myLogClass()
 
 exports.createAccount = function (req, res) {
   // obtengo el usuario del parametro y reviso si tiene el mismo token
-  User.find({
+  User.findOne({
     _id: req.params.user
   }, function (error, user) {
     if (error) {
@@ -24,15 +24,37 @@ exports.createAccount = function (req, res) {
       })
     }
     // obtenido el usuario, verifico si ya tiene una cuenta
-    Account
-    return res.json({
-      message: 'Cuenta agregada correctamente'
+    Account.find({ User: user, type: "facebook" }, function (error, accounts) {
+      if (accounts.length == 0) {
+        //El usuario no tenia una cuenta de facebook
+        var newAccount = new Account();
+        newAccount.type = 'facebook';
+        newAccount.User = user;
+        newAccount.facebook_id = req.body.facebook_id
+        newAccount.save(function (error, account) {
+          if (error) {
+            logger.error('Ocurrio un error al guardar la cuenta');
+            logger.error(error);
+          }
+          return res.status(200).json({
+            message: "Cuenta añadida correctamente",
+            data: account
+          });
+        });
+      }
+      else {
+        return res.status(200).json({
+          message: "Cuenta ya existente",
+          data: accounts[0]
+        });
+      }
     })
   })
 }
 
 exports.getAccounts = function (req, res) {
-  Account.find({User: req.body.user}, function (error, accounts) {
+  
+  Account.find({ User: req.params.user }, function (error, accounts) {
     if (error) {
       logger.error('Ocurrio un error al traer las cuentas')
       logger.error(error)
--- server/models/account.js
+++ server/models/account.js
@@ -6,7 +6,8 @@ var Schema = mongoose.Schema
 // define schema
 var AccountSchema = new Schema({
   type: String,
-  facebook_id: String
+  facebook_id: String,
+  User: { type: Schema.ObjectId, ref: "User" },
 })
 
 module.exports = mongoose.model('Account', AccountSchema)
