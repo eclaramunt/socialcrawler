34cbd8484afa4f86d010f9b09541ed6fada2bf11
Ezequiel Claramunt <ezequiel@sthventures.com>
2017-08-30 14:47:06 -0300

    Un poco de estetica sobre el dash

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Account } from '../_models/account';
+import { fbService } from '../_services/fb.service';
 
 @Component({
   templateUrl: 'home.component.html'
 })
 
 export class HomeComponent implements OnInit {
-  constructor() { }
-
+  constructor(private fbService: fbService) { }
 
   ngOnInit() {
-
+    console.log(this.fbService.me());
   }
 }
